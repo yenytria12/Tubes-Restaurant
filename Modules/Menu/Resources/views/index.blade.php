9dea98137e91b8428ac6f65a343bec35d224fe08
embuntiara <embuntiarar@gmail.com>
2025-01-13 22:10:18 +0700

    apusin yg ga perlu

--- Modules/Menu/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('menu::layouts.master')
-
-@section('content')
-    <h1>MENU RESTORANT D'ENYA</h1>
-
-    <p>
-        This view is loaded from module: {!! config('menu.name') !!}
-    </p>
-@endsection
