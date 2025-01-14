cbbd26e677ad192ca39e2fa7f62e197d9c052998
embuntiara <embuntiarar@gmail.com>
2025-01-14 10:02:07 +0700

    anu

--- database/factories/migrations/2022_05_27_132708_create_menus_table.php
+++ database/factories/migrations/2022_05_27_132708_create_menus_table.php
@@ -4,32 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateMenusTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()    
+    public function up()
     {
-    Schema::create('menus', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->text('description')->nullable();
-        $table->decimal('price', 8, 2);
-        $table->timestamps();
-    });
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
     }
 
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('menus');
     }
-};
+}
+
