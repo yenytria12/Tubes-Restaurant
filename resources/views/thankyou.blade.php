e461689cd4540c60210dc74474b3aa8501a3718d
Desyka <email@widya.com>
2025-01-14 18:50:02 +0700

    perubahan

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -5,13 +5,13 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
-                    <h1 class="heading"> We wil get back to you soon.</h1>
+                    <h1 class="heading"> Kami akan Menunggu Kehadiran Anda Kembali.</h1>
                     <hr>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-offset-2 col-md-8 col-sm-12 text-center">
-                    <h1 class=""> Thank you for your reservation!</h1>
+                    <h1 class=""> Terima Kasih Telah Memesan di Resto Kami!</h1>
                 </div>
             </div>
         </div>
