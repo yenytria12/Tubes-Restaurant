b20938b012ea637b46320dceafa059be925fe7c5
Desyka <email@widya.com>
2025-01-14 19:27:43 +0700

    add

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -1,29 +1,69 @@
-@extends('layouts.app')
+<x-guest-layout>
 
 @section('title', 'Menu - Restaurant Denyah')
 
 @section('content')
     <div class="container mt-5">
         <h2 class="text-center mb-4">Menu Restaurant D'enyah</h2>
-        <div class="row">
+
+        <!-- Kategori Makanan Berat -->
+        <h3>Makanan Berat</h3>
+        <div class="row mb-5">
             @foreach ($menus as $menu)
-                <div class="col-md-4 mb-4">
-                    <div class="card shadow-sm">
-                        <!-- Gambar Menu -->
-                        <img src="{{ asset('images/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->name }}">
-                        <div class="card-body">
-                            <!-- Nama Menu -->
-                            <h5 class="card-title">{{ $menu->name }}</h5>
-                            <!-- Deskripsi Menu -->
-                            <p class="card-text">{{ $menu->description }}</p>
-                            <!-- Harga Menu -->
-                            <p class="card-text"><strong>Rp {{ number_format($menu->price, 0, ',', '.') }}</strong></p>
-                            <!-- Tombol "Add to Cart" -->
-                            <a href="{{ route('order.add', $menu->id) }}" class="btn btn-primary">Add to Cart</a>
+                @if ($menu->category == 'Makanan Berat')
+                    <div class="col-md-4 mb-4">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset('images/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $menu->name }}</h5>
+                                <p class="card-text">{{ $menu->description }}</p>
+                                <p class="card-text"><strong>Rp {{ number_format($menu->price, 0, ',', '.') }}</strong></p>
+                                <a href="{{ route('order.add', $menu->id) }}" class="btn btn-outline-primary">+</a>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endif
+            @endforeach
+        </div>
+
+        <!-- Kategori Makanan Penutup -->
+        <h3>Makanan Penutup</h3>
+        <div class="row mb-5">
+            @foreach ($menus as $menu)
+                @if ($menu->category == 'Makanan Penutup')
+                    <div class="col-md-4 mb-4">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset('images/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $menu->name }}</h5>
+                                <p class="card-text">{{ $menu->description }}</p>
+                                <p class="card-text"><strong>Rp {{ number_format($menu->price, 0, ',', '.') }}</strong></p>
+                                <a href="{{ route('order.add', $menu->id) }}" class="btn btn-outline-primary">+</a>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        </div>
+
+        <!-- Kategori Minuman -->
+        <h3>Minuman</h3>
+        <div class="row mb-5">
+            @foreach ($menus as $menu)
+                @if ($menu->category == 'Minuman')
+                    <div class="col-md-4 mb-4">
+                        <div class="card shadow-sm">
+                            <img src="{{ asset('images/' . $menu->image) }}" class="card-img-top" alt="{{ $menu->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $menu->name }}</h5>
+                                <p class="card-text">{{ $menu->description }}</p>
+                                <p class="card-text"><strong>Rp {{ number_format($menu->price, 0, ',', '.') }}</strong></p>
+                                <a href="{{ route('order.add', $menu->id) }}" class="btn btn-outline-primary">+</a>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             @endforeach
         </div>
     </div>
-@endsection
+</x-guest-layout>
